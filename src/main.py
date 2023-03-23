b4c63f9d72449520fce4f7def84e65e79e06c729
Antonio Díaz Longueira <ajdl0512@gmail.com>
2023-03-23 17:06:09 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,49 +1,16 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
 import datos
-import metricaserror as error
 
-from sklearn import preprocessing, neighbors
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import cross_validate
-from sklearn.pipeline import make_pipeline
-
 from modelosregresion import modelo
+
+path = "./data/output.csv"
+[etiquetas, predictores] = datos.importacionDatos(path)
 
 scoring = ['max_error', 'neg_mean_absolute_error', 'neg_root_mean_squared_error', 'neg_median_absolute_error', 'r2']
 errores = ['Error máximo', 'MAE', 'MSE', 'MAE', 'Coeficiente de correlación']
 
-# modelos = []
-# modelos.append(('LinR', LinearRegression()))
-# n_neighbors = 5
-# modelos.append(('KNN', neighbors.KNeighborsRegressor(n_neighbors)))
+miModelo1 = modelo(LinearRegression())
 
-path = "./data/output.csv"
 
-[etiquetas, predictores] = datos.importacionDatos(path)
-
-miModelo = modelo()
-scores = miModelo.validacionCruzada(predictores, etiquetas, scoring= scoring)
-print(scores)
-
-# CV = 10
-# for (nombre, modelo) in modelos:
-#     modelo = make_pipeline(preprocessing.StandardScaler(), modelo)
-# scores = cross_validate(miModelo, predictores, etiquetas, cv=CV, scoring=scoring)
-#     for i, test in enumerate(scoring):
-#         print(f"La media de {nombre} para [{errores[i]}] es: {np.mean(scores['test_' + test])}")
-#     print("")
-
-# plt.style.use('_mpl-gallery')
-# fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-# ax.plot(predictores.x, predictores.y, etiquetas, 'ro')
-
-# U = np.arange(10, 21, 0.5)
-# V = np.arange(10, 21, 0.5)
-# hola = scores['estimator']
-# hola.predict([U, V])
-
-# ax.plot_surface(U, V, Z)
-# plt.show()
+# for valor, test in zip(scoring, errores):
+#     print(f"El {test} vale {np.mean(scores['test_' + valor])}")
