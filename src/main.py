83775292039219a5959b9efe2b052f55920ef58c
Antonio Díaz Longueira <ajdl0512@gmail.com>
2023-03-23 19:19:47 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,17 @@
+import numpy as np
+import pandas as pd
+
 import datos
+import graficas
 
 from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.svm import SVR
+from sklearn.tree import DecisionTreeRegressor
+
+import tensorflow as tf
+from keras import layers
+
 from modelosregresion import modelo
 
 path = "./data/output.csv"
@@ -9,8 +20,29 @@ path = "./data/output.csv"
 scoring = ['max_error', 'neg_mean_absolute_error', 'neg_root_mean_squared_error', 'neg_median_absolute_error', 'r2']
 errores = ['Error máximo', 'MAE', 'MSE', 'MAE', 'Coeficiente de correlación']
 
-miModelo1 = modelo(LinearRegression())
+RL = modelo(LinearRegression())
+KNN = modelo(KNeighborsRegressor())
+SVM = modelo(SVR())
+DT = modelo(DecisionTreeRegressor())
 
+modelos = []
+modelos.append(RL)
+# modelos.append(KNN)
+# modelos.append(SVM)
+# modelos.append(DT)
 
-# for valor, test in zip(scoring, errores):
-#     print(f"El {test} vale {np.mean(scores['test_' + valor])}")
+for nombre, tecnica in zip(["Regresión Lineal", "K vecinos más cercanos", "SVM", "Árbol de decisión"], modelos):
+    print(nombre)
+    scores = tecnica.validacionCruzada(predictores, etiquetas)
+    for test, valor in zip(errores, scoring):
+        print(f"El {test} vale {np.mean(scores['test_' + valor])}")
+    print("")
+
+MAE = (RL.scores)
+nombre = np.ones((10, 1))
+nModelo = np.arange(1, 11)
+
+datos = np.column_stack((MAE, nombre, nModelo))
+
+df = pd.DataFrame(datos, columns= ['MAE', 'Metodo', 'Iteracion'])
+graficas.boxplot(data= df, x= 'Iteracion', y= 'MAE')
