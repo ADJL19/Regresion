6a3a570e9a90faa7667ee5364e8c41e5c2626e02
Antonio Díaz Longueira <ajdl0512@gmail.com>
2023-03-23 19:19:30 +0100

    Update modelosregresion.py

--- src/modelosregresion.py
+++ src/modelosregresion.py
@@ -2,12 +2,16 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import cross_validate
 
 class modelo():
-    def __init__(self, modelo):
+    def __init__(self, modelo= LinearRegression()):
+        self.__modelo = modelo
         self.__prediccion = []
         self.__scores = []
-        self.__modelo = modelo
         self.__scoring = ['max_error', 'neg_mean_absolute_error', 'neg_root_mean_squared_error', 'neg_median_absolute_error', 'r2']
         self.__CV = 10
+
+    @property
+    def hiperparametros(self):
+        return self.__modelo.get_params(False)
 
     def entrenarModelo(self, train, t_train):
         self.__modelo = self.__modelo.fit(train, t_train)
@@ -20,22 +24,10 @@ class modelo():
     def prediccion(self):
         return self.__prediccion
     
-    @property
-    def hiperparametros(self):
-        return self.__modelo.get_params(False)
-    
     def validacionCruzada(self, predictores, etiquetas):
         self.__scores = cross_validate(self.__modelo, predictores, etiquetas, scoring= self.__scoring, cv= self.__CV)
         return self.__scores
-
-def main():
-    juan = modelo(LinearRegression())
-    predictores = [[0, 0], [1, 1], [2, 2], [3, 3], [4, 4]]
-    etiquetas = [[1], [2], [3], [4], [5]]
-
-    juan.entrenarModelo(predictores, etiquetas)
-    print(juan.predecir(predictores))
-    print(juan.hiperparametros)
-
-if __name__ == "__main__":
-    main()
+    
+    @property
+    def scores(self):
+        return self.__scores['test_neg_mean_absolute_error']
