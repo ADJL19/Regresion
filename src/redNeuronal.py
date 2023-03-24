5308146bf1e6dd0fc11927646a9d8090701b2734
Antonio Díaz Longueira <ajdl0512@gmail.com>
2023-03-24 14:32:21 +0100

    Update redNeuronal.py

--- src/redNeuronal.py
+++ src/redNeuronal.py
@@ -2,11 +2,12 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import funciones
 import pandas as pd
+import numpy as np
 
 from sklearn.model_selection import KFold
 
 #Función encargada de la creación de una red neunoral feedforward.
-def crearNN(n_entradas, metricas, NO= [], FA= 'relu', LR= 0.01, LF= "mean_squared_error"):
+def crearNN(n_entradas, metricas, NO= [10], FA= 'relu', LR= 0.01, LF= "mean_squared_error"):
     #Creamos la estructura de la RN.
     modelo = tf.keras.models.Sequential()
     #Definimos primero la capa de entrada de la red neuronal.
@@ -33,10 +34,8 @@ def validacionCruzada(predictores, target, metricas):
     #Realizamos la subdivisón de los datos en K grupos.
     kf = KFold(n_splits= K)
 
-    #Creamos un DataFrame con las columnas de las métricas.
-    df = pd.DataFrame(columns=metricas)
-
-
+    #Creamos un DataFrame con las columnas de las métricas y otro con la técnica y la iteración.
+    df = pd.DataFrame(columns= metricas)
     for i, (train_index, test_index) in enumerate(kf.split(predictores, target)):
         #Dividimos los datos de entrada y salida de la RN, tanto para el entrenamiento como para el test.
         X_train, t_train = predictores[train_index], target[train_index]
@@ -49,10 +48,12 @@ def validacionCruzada(predictores, target, metricas):
             # history = model.fit(X_train, t_train, validation_data=(X_test, t_test), epochs=epochs, batch_size=40, verbose=0)
         model.fit(X_train, t_train, validation_data=(X_test, t_test), epochs=epochs, batch_size=40, verbose=0)
 
-        #Cargamos en el DataFrame el valor de las métricas seleccionadas.
-        df.loc[i] = model.evaluate(X_test, t_test, batch_size=None)[1:]  
+        #Cargamos en el DataFrame el valor de las métricas seleccionadas, junto a la técnica aplicada y la iteración.
+        # df.loc[i] = [model.evaluate(X_test, t_test, batch_size=None)[1:], 5.0, i]
+        df.loc[i] = model.evaluate(X_test, t_test, batch_size=None)[1:]
 
     # return results, history
+
     return df
 
 def main():
