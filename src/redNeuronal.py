b9c001f8a9d46b93f90a8bc9bf130924bfff1336
Antonio Díaz Longueira <ajdl0512@gmail.com>
2023-03-24 14:02:29 +0100

    Update redNeuronal.py

--- src/redNeuronal.py
+++ src/redNeuronal.py
@@ -5,61 +5,63 @@ import pandas as pd
 
 from sklearn.model_selection import KFold
 
-def crearNN(n_entradas, metricas, HN= [10], FA= 'relu', LR= 0.01):
+#Función encargada de la creación de una red neunoral feedforward.
+def crearNN(n_entradas, metricas, NO= [], FA= 'relu', LR= 0.01, LF= "mean_squared_error"):
+    #Creamos la estructura de la RN.
     modelo = tf.keras.models.Sequential()
+    #Definimos primero la capa de entrada de la red neuronal.
     modelo.add(tf.keras.layers.Input(shape=(n_entradas, )))
-    for NO in HN:
-        modelo.add(tf.keras.layers.Dense(NO, activation= FA))
+    #Definimos las capas ocultas de la red con HN neuronas. El número de capas ocultas va implícito en el número de elementos de la lista NO.
+    for HN in NO:
+        #Se añade cada capa con su número de neuronas y la función de activación seleccionada.
+        modelo.add(tf.keras.layers.Dense(HN, activation= FA))
+    #Añadimos, por último, la capa de salida. Solo posee una neurona con una función de activación lineal, puesto que es un problema de regresión.
     modelo.add(tf.keras.layers.Dense(1, activation= 'linear'))
 
+    #Establecemos el optimizador con su tasa de aprendizaje LR.
     opt = tf.keras.optimizers.Adam(learning_rate= LR)
-    modelo.compile(loss="mean_squared_error", optimizer=opt, metrics=metricas)
 
+    #Por último, seleccionamos las características del proceso que va a seguir el entrenamiento. Por defecto, se emplea una función de error de MSE.
+    modelo.compile(loss=LF, optimizer=opt, metrics=metricas)
     return modelo
 
-def validacionCruzada(predictores, etiquetas, metricas):
-    k = 10
-    epochs = 200
+#Función encargada de realizar la validación cruzada para la RN.
+def validacionCruzada(predictores, target, metricas):
+    #Establecemos el número de grupos y de iteraciones para la validación cruzada.
+    K, epochs = 10, 200
 
-    kf = KFold(n_splits= k)
-    results = pd.DataFrame(columns=metricas)
+    #Realizamos la subdivisón de los datos en K grupos.
+    kf = KFold(n_splits= K)
 
-    for i, (train_index, test_index) in enumerate(kf.split(predictores, etiquetas)):
-        print('k_fold', i+1, 'de', k)
+    #Creamos un DataFrame con las columnas de las métricas.
+    df = pd.DataFrame(columns=metricas)
 
-        # Se obtienen los paquetes de datos de entrenamiento y test en base a los índices aleatorios generados en la k-fold
-        X_train, t_train = predictores[train_index], etiquetas[train_index]
-        X_test, t_test = predictores[test_index], etiquetas[test_index]
 
-        # Se carga el modelo en cada paso de la kfold para resetear el entrenamiento (pesos)
+    for i, (train_index, test_index) in enumerate(kf.split(predictores, target)):
+        #Dividimos los datos de entrada y salida de la RN, tanto para el entrenamiento como para el test.
+        X_train, t_train = predictores[train_index], target[train_index]
+        X_test, t_test = predictores[test_index], target[test_index]
+
+        #Recargamos el modelo para resetear el valor de los pesos.
         model = crearNN(n_entradas= 2, metricas= metricas)  
 
-        # Se realiza el entrenamiento de la red de neuronas
-        history = model.fit(X_train, t_train, validation_data=(X_test, t_test), epochs=epochs, batch_size=40, verbose=0)
+        #Entrenamos el modelo de la red neuronal.
+            # history = model.fit(X_train, t_train, validation_data=(X_test, t_test), epochs=epochs, batch_size=40, verbose=0)
+        model.fit(X_train, t_train, validation_data=(X_test, t_test), epochs=epochs, batch_size=40, verbose=0)
 
-        # Se añade una línea en la tabla de resultados (dataframe de pandas) con los resultados de las métricas seleccionadas
-        results.loc[i] = model.evaluate(X_test, t_test, batch_size=None)[1:]  
+        #Cargamos en el DataFrame el valor de las métricas seleccionadas.
+        df.loc[i] = model.evaluate(X_test, t_test, batch_size=None)[1:]  
 
-    return results, history
+    # return results, history
+    return df
 
 def main():
     metricas = ["mean_squared_error", "mean_absolute_error"]
-    path="./data/output.csv"
-    [etiquetas, predictores] = funciones.importacionDatos(path)
-    
-    # model = crearNN(2, metricas= metricas)
+    path = "./data/output.csv"
+    [target, predictores] = funciones.importacionDatos(path)
 
-    [r, history] = validacionCruzada(predictores, etiquetas, metricas)
-
+    r = validacionCruzada(predictores, target, metricas)
     print(r)
-
-    # plt.plot(history.history['mean_squared_error'])
-    # plt.plot(history.history['val_mean_squared_error'])
-    # plt.title('Métrica de error')
-    # plt.ylabel('MSE')
-    # plt.xlabel('Iteración (epoch)')
-    # plt.legend(['Entrenamiento', 'Test'], loc='lower right')
-    # plt.show()
 
 if __name__ == "__main__":
     main()
